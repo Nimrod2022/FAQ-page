36edcc0586d9c93f51517597469f9a1bf675ec90
Nimrod Kibet <nimrodmandelakibet@gmail.com>
2024-02-21 14:03:32 +0100

    text and button clicks

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-'use strict'
+"use strict";
 
-const expandButton = document.getElementById('expand');
-const buttonImage = expandButton.querySelector('img');
-
+const expandButton = document.getElementById("expand");
+const buttonImage = expandButton.querySelector("img");
+const questionsClick = document.getElementById("question");
 const minimizeButtonImage = "./assets/images/icon-minus.svg";
 const expandButtonImage = "./assets/images/icon-plus.svg";
 
-
-// Track button clicks
 let clicked = false;
 
-// Change expand and close icon
-expandButton.addEventListener('click', function() {
-    if (clicked) {                     
-        buttonImage.src = expandButtonImage;
-        clicked = false; 
-    } else {        
-        buttonImage.src = minimizeButtonImage;
-        clicked = true; 
+
+function handleClicks(clickElementId) {
+  clickElementId.addEventListener("click", function () {
+    if (clicked) {
+      buttonImage.src = expandButtonImage;
+      clicked = false;
+    } else {
+      buttonImage.src = minimizeButtonImage;
+      clicked = true;
     }
-});
+  });
+}
+
+handleClicks(expandButton);
+handleClicks(questionsClick);
