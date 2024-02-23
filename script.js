af7f1cfe37e13cc62b69c4e000d1b6e39d87b7ce
Nimrod Kibet <nimrodmandelakibet@gmail.com>
2024-02-23 12:46:43 +0100

    toggling implemented

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// Exand minimize image src
+
+const expandImage = "./assets/images/icon-plus.svg";
+const minimizeImage = "./assets/images/icon-minus.svg";
+
+// All clickable elements for toggling
+const toggleElements = document.querySelectorAll(".expand > img, .expand > h5");
+
+toggleElements.forEach((toggle) => {
+  toggle.addEventListener("click", function () {
+
+    const content = toggle.parentNode.nextElementSibling;
+    // Ccheck hidden status
+    const isShown = !content.classList.contains("hidden");
+    // change toggle image
+    toggle.src = isShown ? expandImage : minimizeImage;
+    content.classList.toggle("hidden");
+  });
+});
